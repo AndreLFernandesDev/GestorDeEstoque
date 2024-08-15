0efa45f864240df939023551d493fe4f823b738e
André <dedeluiz3040@gmail.com>
2024-08-14 23:14:05 -0300

    feat: Adicionado pasta Models e Migrations para criar tabela Users

---
+++ GestorDeEstoque/Models/User.cs
@@ -0,0 +1,16 @@
+namespace GestorDeEstoque.Models
+{
+  public class Users
+  {
+    public int Id { get; private set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Users(int id, string name, int age)
+    {
+      Id = id;
+      Name = name ?? throw new ArgumentNullException(nameof(name));
+      Age = age;
+    }
+  }
+}
