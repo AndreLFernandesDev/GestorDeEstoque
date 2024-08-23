131e5e4cfcbd091e539ebef8bfa502c4df2fa213
André <dedeluiz3040@gmail.com>
2024-08-22 22:54:22 -0300

    feat: adicionado registro de serviço do UserRepository

--- GestorDeEstoque/Program.cs
+++ GestorDeEstoque/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using GestorDeEstoque.Data;
 using GestorDeEstoque.Repositories;
+using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
@@ -9,6 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 
+builder.Services.AddScoped<UserRepository>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
