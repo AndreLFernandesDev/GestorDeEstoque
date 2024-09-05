d399961fce7502475b0488eae0a2c6f15f87e829
André <andrelfernandesdev@gmail.com>
2024-09-04 22:59:52 -0300

    feat: Criar tabela estoque, relacionar: Estoque -> Produto

---
+++ GestorDeEstoque/Migrations/20240905015401_RelacionarTabelaEstoqueEProduto.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace GestorDeEstoque.Migrations
+{
+    /// <inheritdoc />
+    public partial class RelacionarTabelaEstoqueEProduto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EstoqueId",
+                table: "Produtos",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Quantidade",
+                table: "Produtos",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.CreateTable(
+                name: "Estoques",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estoques", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_EstoqueId",
+                table: "Produtos",
+                column: "EstoqueId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Produtos_Estoques_EstoqueId",
+                table: "Produtos",
+                column: "EstoqueId",
+                principalTable: "Estoques",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Produtos_Estoques_EstoqueId",
+                table: "Produtos");
+
+            migrationBuilder.DropTable(
+                name: "Estoques");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Produtos_EstoqueId",
+                table: "Produtos");
+
+            migrationBuilder.DropColumn(
+                name: "EstoqueId",
+                table: "Produtos");
+
+            migrationBuilder.DropColumn(
+                name: "Quantidade",
+                table: "Produtos");
+        }
+    }
+}
---
+++ GestorDeEstoque/Models/Estoque.cs
@@ -0,0 +1,16 @@
+namespace GestorDeEstoque.Models
+{
+    public class Estoque
+    {
+        public int Id { get; set; }
+        public required string Nome { get; set; }
+        public virtual ICollection<Produto> Produtos { get; set; } = [];
+
+
+        public Estoque(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+    }
+}
