5a016b699e6b44ed6433c76680fa203f5c112ff0
André <dedeluiz3040@gmail.com>
2024-09-04 22:59:52 -0300

    feat: Criar tabela estoque, relacionar: Estoque -> Produto

--- GestorDeEstoque/Models/Produto.cs
+++ GestorDeEstoque/Models/Produto.cs
@@ -6,13 +6,18 @@ namespace GestorDeEstoque.Models
         public required string Nome { get; set; }
         public string Descricao { get; set; }
         public decimal Preco { get; set; }
+        public required decimal Quantidade { get; set; }
+        public required int EstoqueId { get; set; }
+        public virtual Estoque Estoque { get; set; } = null!;
 
-        public Produto(int id, string nome, string descricao, decimal preco)
+        public Produto(int id, string nome, string descricao, decimal preco, decimal quantidade, int estoqueId)
         {
             Id = id;
             Nome = nome;
             Descricao = descricao;
             Preco = preco;
+            Quantidade = quantidade;
+            EstoqueId = estoqueId;
         }
 
     }
