29d7604cc2797259b3bf05860baf280f4de1bbb7
André <dedeluiz3040@gmail.com>
2024-09-25 12:23:03 -0300

    feat: Criado endpoints de adicionar e buscar produto

---
+++ GestorDeEstoque/Controllers/ProdutoController.cs
@@ -0,0 +1,53 @@
+using GestorDeEstoque.Models;
+using GestorDeEstoque.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using GestorDeEstoque.DTOs;
+namespace GestorDeEstoque.Controllers
+{
+    [ApiController]
+    [Route("/produtos")]
+    public class ProdutoController : ControllerBase
+    {
+        private ProdutoRepository _produtoRepository;
+        public ProdutoController(ProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository ?? throw new ArgumentNullException(nameof(produtoRepository));
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarProdutoPorId(int id)
+        {
+            var produto = _produtoRepository.BuscarProdutoPorId(id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(produto);
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult InserirProduto([FromBody] ProdutoDTO novoProdutoDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var produto = new Produto
+            {
+
+                Nome = novoProdutoDTO.Nome,
+                Descricao = novoProdutoDTO.Descricao,
+                Preco = novoProdutoDTO.Preco,
+                Quantidade = novoProdutoDTO.Quantidade,
+                EstoqueId = novoProdutoDTO.EstoqueId
+            };
+            _produtoRepository.InserirProduto(produto);
+
+            return CreatedAtAction(nameof(BuscarProdutoPorId), new { id = produto.Id }, produto);
+        }
+    }
+}
