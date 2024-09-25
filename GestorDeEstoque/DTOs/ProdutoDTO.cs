62e718a76336e01d1316fe6f0b70a73a4d098201
André <dedeluiz3040@gmail.com>
2024-09-25 12:20:19 -0300

    feat: Criado ProdutoDTO, para que seja passado somente propriedades obrigatórias do produto

---
+++ GestorDeEstoque/DTOs/ProdutoDTO.cs
@@ -0,0 +1,11 @@
+namespace GestorDeEstoque.DTOs
+{
+    public class ProdutoDTO
+    {
+        public required string Nome { get; set; }
+        public required string Descricao { get; set; }
+        public required decimal Preco { get; set; }
+        public required decimal Quantidade { get; set; }
+        public required int EstoqueId { get; set; }
+    }
+}
