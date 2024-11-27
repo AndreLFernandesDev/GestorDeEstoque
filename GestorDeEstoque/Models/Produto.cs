b106cf55c34cea7c45fed0687a48c4ed78c97ee1
André <dedeluiz3040@gmail.com>
2024-11-27 00:18:44 -0300

    chore: removido propriedades quantidade e estoqueId que foram transferidas para classe ProdutoEstoque

--- GestorDeEstoque/Models/Produto.cs
+++ GestorDeEstoque/Models/Produto.cs
@@ -3,11 +3,9 @@ namespace GestorDeEstoque.Models
     public class Produto
     {
         public int Id { get; private set; }
-        public string Nome { get; set; } = null!;
-        public string Descricao { get; set; } = null!;
-        public decimal Preco { get; set; }
-        public decimal Quantidade { get; set; }
-        public int EstoqueId { get; set; }
+        public required string Nome { get; set; } = null!;
+        public required string Descricao { get; set; } = null!;
+        public required decimal Preco { get; set; }
         public virtual ICollection<LogEstoque> LogsEstoque { get; set; } = [];
         public virtual ICollection<ProdutoEstoque> ProdutosEstoques { get; set; } = [];
     }
