64f7abe5dc8eae8756cc6ad73e0a80f4fe10e7f7
André <dedeluiz3040@gmail.com>
2024-12-09 23:28:17 -0300

    chore: removido retornos desnecessários, trocado nomes de variáveis e métodos, alterado rota do método AtualizarQuantidadeProdutoAsync para melhor coerência

--- GestorDeEstoque/Repositories/IEstoqueRepository.cs
+++ GestorDeEstoque/Repositories/IEstoqueRepository.cs
@@ -3,6 +3,6 @@ namespace GestorDeEstoque.Repositories
 {
     public interface IEstoqueRepository
     {
-        public Task<ProdutoEstoque> AtualizarQuantidadeProdutoAsync(int idProduto, decimal quantidade, int idEstoque);
+        public Task<ProdutoEstoque> AtualizarQuantidadeProdutoAsync(int idEstoque, decimal quantidade, int idProduto);
     }
 }
--- GestorDeEstoque/Repositories/IProdutoEstoqueRepository.cs
+++ GestorDeEstoque/Repositories/IProdutoEstoqueRepository.cs
@@ -3,8 +3,8 @@ namespace GestorDeEstoque.Repositories
 {
     public interface IProdutoEstoqueRepository
     {
-        public Task<ProdutoEstoque> BuscarQuantidadePorIdAsync(int idProduto, int idEstoque);
-        public Task<ProdutoEstoque> AdicionarQuantidadeAsync(ProdutoEstoque produtoEstoque);
+        public Task<ProdutoEstoque> BuscarQuantidadePorIdEstoqueEhIdProdutoAsync(int idProduto, int idEstoque);
+        public Task AdicionarQuantidadeAsync(ProdutoEstoque produtoEstoque);
         public Task<bool> RemoverQuantidadeProdutoAsync(int idProduto, int idEstoque);
     }
 }
