e8b21585f141d92ddc7a68a523ea22cd1586e96a
André <dedeluiz3040@gmail.com>
2024-12-09 23:39:44 -0300

    fix: adicionado propriedade virtual EstoqueId, adicionado propriedades virtual e garante que não serão nulas

--- GestorDeEstoque/Models/ProdutoEstoque.cs
+++ GestorDeEstoque/Models/ProdutoEstoque.cs
@@ -5,10 +5,10 @@ namespace GestorDeEstoque.Models
     public class ProdutoEstoque
     {
         public int ProdutoId { get; set; }
-        public Produto? Produto { get; set; }
+        public virtual Produto Produto { get; set; } = null!;
 
         public int EstoqueId { get; set; }
-        public Estoque? Estoque { get; set; }
+        public virtual Estoque Estoque { get; set; } = null!;
 
         public decimal Quantidade { get; set; }
     }
