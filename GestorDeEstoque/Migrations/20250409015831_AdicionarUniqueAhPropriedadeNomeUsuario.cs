cfa0e1f4192298632ddbf172a7c500a8c4304cca
André <andrelfernandesdev@gmail.com>
2025-04-08 23:16:16 -0300

    feat: Criada migrations para a tabela de usuário

---
+++ GestorDeEstoque/Migrations/20250409015831_AdicionarUniqueAhPropriedadeNomeUsuario.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestorDeEstoque.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionarUniqueAhPropriedadeNomeUsuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuarios_NomeUsuario",
+                table: "Usuarios",
+                column: "NomeUsuario",
+                unique: true
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(name: "IX_Usuarios_NomeUsuario", table: "Usuarios");
+        }
+    }
+}
