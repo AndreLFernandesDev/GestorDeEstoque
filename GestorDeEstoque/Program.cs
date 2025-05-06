6004f678e3a77c3438b0a2e96b5fb3fee88728a6
André <andrelfernandesdev@gmail.com>
2025-05-06 11:34:55 -0300

    feat: Implementado login e criação de token

--- GestorDeEstoque/Program.cs
+++ GestorDeEstoque/Program.cs
@@ -1,10 +1,21 @@
+using System.Text;
 using System.Text.Json.Serialization;
 using GestorDeEstoque.Data;
 using GestorDeEstoque.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var jwtKey = builder.Configuration["Jwt:Key"] ?? string.Empty;
+var jwtIssuer = builder.Configuration["Jwt:Issuer"] ?? string.Empty;
+var jwtAudience = builder.Configuration["Jwt:Audience"] ?? string.Empty;
+var jwtExpireHours = int.TryParse(builder.Configuration["Jwt:ExpireHours"], out var hours)
+    ? hours
+    : 2;
 
 // Configuração dos serviços
 builder
@@ -19,6 +30,29 @@ builder.Services.AddScoped<EstoqueRepository>();
 builder.Services.AddScoped<LogRepository>();
 builder.Services.AddScoped<ProdutoEstoqueRepository>();
 builder.Services.AddScoped<UsuarioRepository>();
+
+// Configuração de autenticação
+builder
+    .Services.AddAuthentication(Options =>
+    {
+        Options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        Options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(Options =>
+    {
+        Options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtIssuer,
+            ValidAudience = jwtAudience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey)),
+        };
+    });
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
